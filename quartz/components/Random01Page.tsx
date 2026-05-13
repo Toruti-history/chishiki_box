533b64ad03ac6d869b6f93e68efa313c87c7fb5e
Toruti-history <toruti42@gmail.com>
2026-05-13 23:59:03 +0900

    test

--- quartz/components/Random01Page.tsx
+++ quartz/components/Random01Page.tsx
@@ -4,34 +4,8 @@ const Random01Page: QuartzComponentConstructor = () => {
   return () => (
     <button
       type="button"
-      onClick={async () => {
-        try {
-          const res = await fetch("/static/contentIndex.json")
-          const data = await res.json()
-
-          console.log(data)
-
-          const pages = Object.values(data)
-            .map((item: any) => item.slug)
-            .filter((slug) => slug?.startsWith("01Page/"))
-
-          console.log(pages)
-
-          if (pages.length === 0) {
-            alert("No pages found")
-            return
-          }
-
-          const randomPage =
-            pages[Math.floor(Math.random() * pages.length)]
-
-          console.log(randomPage)
-
-          window.location.href = "/" + randomPage
-
-        } catch (err) {
-          console.error(err)
-        }
+      onClick={() => {
+        alert("clicked")
       }}
     >
       Random Note
